16ed4d897290e49f9505b53da15cce66ac78277d
rsarkar-test <53563278+rsarkar-test@users.noreply.github.com>
2020-09-29 22:22:52 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,16 @@
 using System;
-using System.Threading.Tasks;
-
+using System.Threading;
 namespace NetCore.Docker
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            var counter = 0;
-            var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
-            while (max == -1 || counter < max)
-            {
-                Console.WriteLine($"Counter: {++counter}");
-                await Task.Delay(1000);
-            }
+		    while(true)
+			{
+            Console.WriteLine("Welcome to Devops !!");
+			Thread.Sleep(2000);
+			}
         }
     }
 }
